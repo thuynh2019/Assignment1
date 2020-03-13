3ebcd5cd098578d89fda179444a1879be29e4149
thuynh2019 <ty.huynh@smail.astate.edu>
2020-03-12 23:40:36 -0500

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var coolWords: String = "Hello, playground."
+
+var integer = 10
+var doubleTester: Double = 10.053242
+
+var string2 = "Hi there!"
+
+var integer2 = 20
+
+coolWords
+integer
+integer2
+integer + integer2
+integer * integer2
+integer2 / integer
+
+coolWords + " " + string2
+coolWords = String(integer)
