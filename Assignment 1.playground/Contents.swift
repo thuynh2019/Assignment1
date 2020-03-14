5de984c67d34ff441b21406d8ee04770a2a1ed60
thuynh2019 <ty.huynh@smail.astate.edu>
2020-03-13 23:29:19 -0500

    Actual Assignment 1

---
+++ Assignment 1.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var name = "Ty Huynh(Tie Hwin)"
+
+var age = "25"
+
+var birthplace = "Rendondo Beach, California"
+
+var birthplace2 = "Los Angeles"
+
+var hobby = "playing videos games"
+
+var hobby2 = "watching Netflix"
+
+var hobby3 = "going on adventures with my dog"
+
+var hobbies = hobby + ", " + hobby2 + ", and " + hobby3
+
+var bio = "Hello! My name is \(name), and I am \(age) years old. \nI was born in \(birthplace)in the \(birthplace2) junction. \nMy favorite things to do when I actually do have time from school and work are: \n\(hobbies)!"
+
+print(bio)
